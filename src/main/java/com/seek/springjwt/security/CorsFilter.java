2a816adb7edfa5c87fa26c09bcadd9ea55574f51
MasterMarandu <derlisg@gmail.com>
2025-02-09 14:56:33 -0300

    Update CorsFilter.java

--- src/main/java/com/seek/springjwt/security/CorsFilter.java
+++ src/main/java/com/seek/springjwt/security/CorsFilter.java
@@ -16,7 +16,8 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:80");
+        response.setHeader("Access-Control-Allow-Origin", "http://159.223.202.94:80");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, Authorization, content-type, xsrf-token");
